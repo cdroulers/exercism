d62cbffe81015e1c36591ffd1998540db9e78873
cdroulers <droulers.christian@gmail.com>
2014-07-28 10:09:15 -0400

    Initial commit with csharp/leap solved.

---
+++ csharp/leap/Year.cs
@@ -0,0 +1,14 @@
+﻿public static class Year
+{
+    public static bool IsLeap(int year)
+    {
+        return year.IsDivisibleBy(4) &&
+            (!year.IsDivisibleBy(100) ||
+            year.IsDivisibleBy(400));
+    }
+
+    private static bool IsDivisibleBy(this int number, int divisor)
+    {
+        return number % divisor == 0;
+    }
+}
