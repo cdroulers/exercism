c852e23adeb6f3323629743ec380a5dd54589a56
cdroulers <droulers.christian@gmail.com>
2014-08-13 18:50:49 -0400

    First iteration of csharp/phone-number.

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -1,4 +1,41 @@
+using System.Text.RegularExpressions;
+
 public class PhoneNumber
 {
-	
+	private readonly string numbers;
+	private readonly string areaCode;
+
+	private static readonly Regex NumbersOnlyRegex = new Regex(@"[^0-9]");
+
+	public PhoneNumber(string numbers)
+	{
+		var onlyNumbers = PhoneNumber.NumbersOnlyRegex.Replace(numbers, string.Empty);
+
+		if (onlyNumbers.Length == 11 && onlyNumbers[0] == '1')
+		{
+			onlyNumbers = onlyNumbers.Substring(1);
+		}
+		else if (onlyNumbers.Length != 10)
+		{
+			onlyNumbers = "0000000000";
+		}
+
+		this.numbers = onlyNumbers;
+		this.areaCode = onlyNumbers.Substring(0, 3);
+	}
+
+	public string Number
+	{
+		get { return this.numbers; }
+	}
+
+	public string AreaCode
+	{
+		get { return this.areaCode; }
+	}
+
+	public override string ToString()
+	{
+		return string.Format("({0}) {1}-{2}", this.areaCode, this.numbers.Substring(3, 3), this.numbers.Substring(6, 4));
+	}
 }
