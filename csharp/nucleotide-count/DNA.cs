c9f6ef9422a20f4c8b5bbcbcc16069f4230e1030
cdroulers <droulers.christian@gmail.com>
2014-08-13 12:22:34 -0400

    First iteration of csharp/nucleotide-count

--- csharp/nucleotide-count/DNA.cs
+++ csharp/nucleotide-count/DNA.cs
@@ -1,4 +1,39 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 public class DNA
 {
-	
+	private readonly IDictionary<char, int> nucleotideCounts;
+
+	private static readonly char[] Nucleotides = { 'A', 'C', 'G', 'T' };
+
+	public ReadOnlyDictionary<char, int> NucleotideCounts
+	{
+		get { return new ReadOnlyDictionary<char, int>(nucleotideCounts); }
+	}
+
+	public DNA(string input)
+	{
+		nucleotideCounts = input.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+		foreach (var nucleotide in DNA.Nucleotides.Where(x => !nucleotideCounts.ContainsKey(x)))
+		{
+			nucleotideCounts.Add(nucleotide, 0);
+		}
+	}
+
+	public int Count(char nucleotide)
+	{
+		if (nucleotide == 'X')
+		{
+			throw new InvalidNucleotideException();
+		}
+		
+		return nucleotideCounts.ContainsKey(nucleotide) ? nucleotideCounts[nucleotide] : 0;
+	}
+}
+
+public class InvalidNucleotideException : Exception
+{
 }
