2f573769018f777935c8325c9e1ebb14e843cc69
cdroulers <droulers.christian@gmail.com>
2014-09-27 15:20:55 -0400

    Second iteration of python/hamming. Using oh so awesome map reduce and sum function

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,8 +1,4 @@
-def hamming(strand_one, strand_two):
-	diff = abs(len(strand_one) - len(strand_two))
+import operator
 
-	count = 0
-	for index, val in enumerate(strand_one):
-		if len(strand_two) > index and val != strand_two[index]:
-			count += 1
-	return count + diff
+def hamming(strand_one, strand_two):
+	return sum(map(operator.ne, strand_one, strand_two))
