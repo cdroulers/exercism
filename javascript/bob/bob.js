d6094c717c4c9e15a7bb58429659acb5cfc47b21
cdroulers <droulers.christian@gmail.com>
2015-10-07 20:24:17 -0400

    Solved JavaScript bob and hello-world. Added leap.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,31 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    if (this.isSilence(input)) {
+        return "Fine. Be that way!";
+    }
+
+    if (this.isYelling(input)) {
+        return "Woah, chill out!";
+    }
+
+    if (this.isQuestion(input)) {
+        return "Sure.";
+    }
+
+    return "Whatever.";
 };
+
+Bob.prototype.isYelling = function (input) {
+    return input.toUpperCase() === input && /[A-Z]+/.test(input);
+}
+
+Bob.prototype.isSilence = function (input) {
+    return /^\s*$/.test(input);
+}
+
+Bob.prototype.isQuestion = function (input) {
+    return input.indexOf("?") === input.length - 1;
+}
 
 module.exports = Bob;
